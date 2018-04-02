35694aaabcffdce9b0f1ee77f5d54554e2137027
fallinbryan <fallinbryan@ufl.edu>
2018-04-01 20:58:58 -0500

    worked out kinks.

--- SmartArray/main.cpp
+++ SmartArray/main.cpp
@@ -1,7 +1,7 @@
 //Example using a SmartArray class with a vector core.
 
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
 template <class T>
@@ -11,19 +11,37 @@ private:
     int sum;
     int size;
     double average;
-    T array[];
+    T* array;
 
-public:
-
-    SmartArray(initializer_list<T> init) : array(init) {
+    void calculateStats() {
         sum = 0;
         average = 0.0;
-        size = init.size();
-
         for(int i = 0; i < size; i++) {
             sum += array[i];
         }
-        average = sum/array.size();
+        average = sum/size;
+    }
+
+public:
+
+    SmartArray(initializer_list<T> init)  { //initializer list allows array = {x,y,x};
+
+        size = int(init.size());
+        const T* iter = init.begin();
+        array = new T[init.size()];
+        for(int i = 0; i < size; i++){
+            array[i] = iter[i];
+        }
+        calculateStats();
+
+    }
+    ~SmartArray() {
+        delete array;
+    }
+
+    T& operator[] (int index ) {
+        calculateStats();
+        return array[index];
     }
 
     int getSum() {
@@ -32,10 +50,17 @@ public:
     double getAverage() {
         return average;
     }
+    int getSize() {
+        return size;
+    }
 };
 
 int main() {
     SmartArray<int> mySmartArray = {12, 34, 56, 78};
     cout << "Sum: " << mySmartArray.getSum() << " Average: " << mySmartArray.getAverage() << endl;
+    mySmartArray[0] = 5; //demonstrates the smart array behaves like a regular array
+
+    cout << "Sum: " << mySmartArray.getSum() << " Average: " << mySmartArray.getAverage() << endl;
+
     return 0;
 }
