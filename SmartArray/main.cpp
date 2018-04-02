88dada634b8e6d464239d8a3632403259782b949
fallinbryan <fallinbryan@ufl.edu>
2018-04-01 21:08:00 -0500

    Added addtional comments

--- SmartArray/main.cpp
+++ SmartArray/main.cpp
@@ -1,5 +1,4 @@
-//Example using a SmartArray class with a vector core.
-
+//Example of a Smart Array class.
 #include <iostream>
 
 using namespace std;
@@ -25,9 +24,12 @@ private:
 public:
 
     SmartArray(initializer_list<T> init)  { //initializer list allows array = {x,y,x};
-
+        //an array that knows it's own size is a smart array
         size = int(init.size());
+        //this grabs a pointer from the start of the initializer_list
         const T* iter = init.begin();
+        // this 'new' keyword allocates memory from the heap.  we must release when
+        // we terminate the program.  That is done in the class destructor.
         array = new T[init.size()];
         for(int i = 0; i < size; i++){
             array[i] = iter[i];
@@ -35,11 +37,18 @@ public:
         calculateStats();
 
     }
+    // this a destructor.  it makes sure we free up allocated memory so our
+    // program does not have a memory leak and eventually crash the entire computer
     ~SmartArray() {
         delete array;
     }
 
+    //this is operator overloading.  google c++ operator overloading
     T& operator[] (int index ) {
+        // this is the smart bit that won't allow the smart array to access memory it isn't supposed to.
+        if(index >= size) {
+            throw "Array index out of bounds";
+        }
         calculateStats();
         return array[index];
     }
